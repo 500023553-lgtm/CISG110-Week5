33b2d8af51a6cbaa5e93f23992fd7785d723a64f
Issac Jung <500023553@fjuhsd.org>
2026-03-08 14:31:54 -0700

    Godot Minigame 2
    This is the Godot Minigame 2

--- week-5-demo/scripts/toy.gd
+++ week-5-demo/scripts/toy.gd
@@ -3,7 +3,7 @@ extends RigidBody2D
 @onready var _sprite = get_node("Sprite2D")
 
 # STEP 2
-var _bounces = 0.0
+var _bounces = 0
 
 func _ready() -> void:
 	set_contact_monitor(true)
@@ -12,11 +12,11 @@ func _ready() -> void:
 
 func _on_body_entered(_body: Node) -> void:
 	# STEP 3
-	_bounces += 1
+	_bounces = _bounces + 1
 	
 	# STEP 1
 	# Write Step 1 right below this comment. 
-	
+	print(_bounces)
 	
 	%UI._updateBounces(_bounces)
 	
@@ -25,24 +25,29 @@ func _on_body_entered(_body: Node) -> void:
 	var b = _sprite.modulate.b
 	
 	# Step 4
-	
+	r = r + 0.1
 	# Step 5
-	
+	g = g +0.1
 	# Step 7
-	
+	b *= 1.2
 	# Step 8
-		
+	if r > 1.0:
+		r = 0.0
 	# Step 9
-		
+	if g > 1.0:
+		g = 0.0
 	# Step 10
-	
+	if b > 2.0:
+		b = 0.0
 	_sprite.modulate = Color(r, g, b)
 	
 	# Step 6
-	
+	print (_sprite.modulate)
 	
 	# Step 11
+	var brightness = (r + b + g)/3
 	
-	
+	print(brightness)
+	%UI._updateBrightness(brightness)
 	#print(brightness)
 	#%UI._updateBrightness(brightness)
